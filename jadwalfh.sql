09fe253585db06e02f094e6cf9f5f593b2ca34de
Omnytrix <68048642+Omnytrix@users.noreply.github.com>
2022-04-07 10:21:52 +0700

    Add files via upload

---
+++ jadwalfh.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 07, 2022 at 05:19 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jadwalfh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+-- Error reading structure for table jadwalfh.dosen: #1932 - Table 'jadwalfh.dosen' doesn't exist in engine
+-- Error reading data for table jadwalfh.dosen: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `jadwalfh`.`dosen`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+-- Error reading structure for table jadwalfh.jadwal: #1932 - Table 'jadwalfh.jadwal' doesn't exist in engine
+-- Error reading data for table jadwalfh.jadwal: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `jadwalfh`.`jadwal`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+-- Error reading structure for table jadwalfh.tb_user: #1932 - Table 'jadwalfh.tb_user' doesn't exist in engine
+-- Error reading data for table jadwalfh.tb_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `jadwalfh`.`tb_user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
